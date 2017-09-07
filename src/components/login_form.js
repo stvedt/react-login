697ee7311df837a202703bde1f8c67ec9503aca9
Stephen Tvedt <stephen@stephentvedt.com>
2017-09-06 21:10:20 -0400

    simplify user exist conditional. some clean up of consoles and comment.

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -5,16 +5,13 @@ export default class LoginForm extends Component {
     super(props);
 
     this.approvedUsers = { "test@zola.com": "zola#frontend"};
-
     this.state = {
       email: '',
       password: '',
-      loggedIn: false,
       emailValid: true,
       passwordValid: true,
       statusMsg: ''
     };
-
   }
   render() {
     return (
@@ -22,7 +19,6 @@ export default class LoginForm extends Component {
         { this.state.statusMsg  &&
           <div className="alert alert-danger" role="alert">{this.state.statusMsg}</div>
         }
-
         <h2 className="form-signin-heading">Please log in:</h2>
 
         <label className="sr-only" htmlFor="inputEmail" >Email address</label>
@@ -40,8 +36,6 @@ export default class LoginForm extends Component {
 
   onEmailChange = (event) => {
     let email = event.target.value;
-    console.log('Email Changed', email);
-
     this.setState({
       email,
       emailValid: this.isEmailValid(email)
@@ -50,8 +44,6 @@ export default class LoginForm extends Component {
 
   onPasswordChange = (event) => {
     let password = event.target.value;
-    console.log('Password Changed:', password);
-
     this.setState({
       password,
       passwordValid: this.isPasswordValid(password)
@@ -60,7 +52,6 @@ export default class LoginForm extends Component {
 
   isEmailValid(email){
     let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //regex from https://stackoverflow.com/questions/46155/how-to-validate-email-address-in-javascript?answertab=active#tab-top
-    console.log('test email');
     return emailRegex.test(email);
   }
 
@@ -75,30 +66,24 @@ export default class LoginForm extends Component {
   }
 
   isAuthenticatedUser = (email, password)=> {
-    console.log('Authenticating', this.approvedUsers[email] , password);
+    //check password is a match
     return (this.approvedUsers[email] === password);
   }
 
   onFormSubmit = (event) => {
     event.preventDefault();
-    if (this.isApprovedUser(this.state.email) ){
-      //User Exists
-      console.log('User Exists');
-      this.setState({loggedIn: true});
-    } else {
+    if ( !this.isApprovedUser(this.state.email) ){
       //Denied - No User
-      console.log('Denied: User Does Not Exist');
       this.setState({statusMsg: 'Denied: User Does Not Exist'});
-      return;
+      return false;
     }
 
     if (this.isAuthenticatedUser(this.state.email, this.state.password) ){
-      //Authenticated
-      console.log('Authenticated. Enter!');
+      //Authenticated Successfully
       this.setState({statusMsg: ''});
       this.props.action(true);//pass true loggedIn to update main app loggedIn state
     } else {
-      console.log('Denied: Password Is Incorrect');
+      //User Exists but incorrect password
       this.setState({statusMsg: 'Denied: Password Is Incorrect'});
     }
   }
