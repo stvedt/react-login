706582b664f3a10d9dbb8a3f09b0d42070f21fe5
Stephen Tvedt <stephen@stephentvedt.com>
2017-09-06 20:56:51 -0400

    add email regex and status message to show denied/pass fail

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -10,17 +10,23 @@ export default class LoginForm extends Component {
       email: '',
       password: '',
       loggedIn: false,
-      passwordValid: false
+      emailValid: true,
+      passwordValid: true,
+      statusMsg: ''
     };
 
   }
   render() {
     return (
       <form className="form-signin" onSubmit={this.onFormSubmit}>
-        <h2 className="form-signin-heading">Please sign in</h2>
+        { this.state.statusMsg  &&
+          <div className="alert alert-danger" role="alert">{this.state.statusMsg}</div>
+        }
+
+        <h2 className="form-signin-heading">Please log in:</h2>
 
         <label className="sr-only" htmlFor="inputEmail" >Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.onEmailChange} required />
+        <input type="email" id="inputEmail" className={"form-control " + (this.state.emailValid ? '' : 'error')} placeholder="Email address" onChange={this.onEmailChange} required />
 
         <label className="sr-only" htmlFor="inputPassword">Password</label>
         <input type="password" id="inputPassword" placeholder="Password"
@@ -36,7 +42,10 @@ export default class LoginForm extends Component {
     let email = event.target.value;
     console.log('Email Changed', email);
 
-    this.setState({email});
+    this.setState({
+      email,
+      emailValid: this.isEmailValid(email)
+    });
   }
 
   onPasswordChange = (event) => {
@@ -45,12 +54,18 @@ export default class LoginForm extends Component {
 
     this.setState({
       password,
-      passwordValid: this.passwordPass(password)
+      passwordValid: this.isPasswordValid(password)
     });
   }
 
-  passwordPass(password){
-    let passwordRegex = new RegExp('^(?=.{10,})(?=.*)(?=.*[@#*$%^&+=]).*$');// This regex expects a-z either case with a length of at elast 10 and at least one special character of @#*$%^&+=
+  isEmailValid(email){
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; //regex from https://stackoverflow.com/questions/46155/how-to-validate-email-address-in-javascript?answertab=active#tab-top
+    console.log('test email');
+    return emailRegex.test(email);
+  }
+
+  isPasswordValid(password){
+    let passwordRegex = /^(?=.{10,})(?=.*)(?=.*[@#*$%^&+=]).*$/;// This regex expects a-z either case with a length of at elast 10 and at least one special character of @#*$%^&+=
     return passwordRegex.test(password);
   }
 
@@ -59,11 +74,31 @@ export default class LoginForm extends Component {
     return Boolean(this.approvedUsers[email]);
   }
 
+  isAuthenticatedUser = (email, password)=> {
+    console.log('Authenticating', this.approvedUsers[email] , password);
+    return (this.approvedUsers[email] === password);
+  }
+
   onFormSubmit = (event) => {
     event.preventDefault();
     if (this.isApprovedUser(this.state.email) ){
-      console.log('approved!');
+      //User Exists
+      console.log('User Exists');
       this.setState({loggedIn: true});
+    } else {
+      //Denied - No User
+      console.log('Denied: User Does Not Exist');
+      this.setState({statusMsg: 'Denied: User Does Not Exist'});
+      return;
+    }
+
+    if (this.isAuthenticatedUser(this.state.email, this.state.password) ){
+      //Authenticated
+      console.log('Authenticated. Enter!');
+      this.setState({statusMsg: ''});
+    } else {
+      console.log('Denied: Password Is Incorrect');
+      this.setState({statusMsg: 'Denied: Password Is Incorrect'});
     }
   }
 }
